091302e6140d79c7a9f65b3e6cabc04683c6d2a9
Shannon <shannon.caldwell@outlook.com>
2019-05-06 11:31:21 +1000

    init repo project

---
+++ Source/MeatRealm/MeatRealmGameMode.h
@@ -0,0 +1,19 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MeatRealmGameMode.generated.h"
+
+UCLASS(MinimalAPI)
+class AMeatRealmGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AMeatRealmGameMode();
+};
+
+
+
