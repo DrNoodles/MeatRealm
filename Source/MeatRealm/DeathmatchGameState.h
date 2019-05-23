01008315784479d9af6b9925e073be81976ae9e3
Shannon <shannon.caldwell@outlook.com>
2019-05-23 11:21:26 +1000

    Renaming code/blueprints and added new state classes necessary moving forward

---
+++ Source/MeatRealm/DeathmatchGameState.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameStateBase.h"
+#include "DeathmatchGameState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MEATREALM_API ADeathmatchGameState : public AGameStateBase
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/MeatRealm/DeathmatchMyGameStateBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameStateBase.h"
+#include "DeathmatchMyGameStateBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MEATREALM_API ADeathmatchMyGameStateBase : public AGameStateBase
+{
+	GENERATED_BODY()
+	
+};
