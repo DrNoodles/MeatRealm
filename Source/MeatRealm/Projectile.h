1bb6f0217f0fd64d1005affd780cad4393a93b4d
Shannon <shannon.caldwell@outlook.com>
2019-08-13 16:48:16 +1000

    Projectile wont detonate on wall imapact if projectile movement is set to bounce.

--- Source/MeatRealm/Projectile.h
+++ Source/MeatRealm/Projectile.h
@@ -15,63 +15,68 @@ UCLASS()
 class MEATREALM_API AProjectile : public AActor
 {
 	GENERATED_BODY()
-	
-public:	
+
+public:
 	AProjectile();
 
 	void SetHeroControllerId(uint32 HeroContId) { HeroControllerId = HeroContId; }
 
 
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	TSubclassOf<AActor> EffectClass;
+
 
 	UPROPERTY(EditAnywhere, BlueprintReadOnly)
-		TSubclassOf<AActor> EffectClass;
+	float ShotDamage = 1.0f;
 
-	
 	UPROPERTY(EditAnywhere, BlueprintReadOnly)
-		float ShotDamage = 1.0f;
-
-
-	
-	UPROPERTY(EditAnywhere, BlueprintReadOnly)
-		bool bIsAoe = false;
+	bool bIsAoe = false;
 
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, meta = (EditCondition = "bIsAoe"))
-		float InnerRadius = 0;
+	float FuseTime = -1;
 
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, meta = (EditCondition = "bIsAoe"))
-		float OuterRadius = 100;
+	float InnerRadius = 0;
 
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, meta = (EditCondition = "bIsAoe"))
-		float Falloff = 1;
+	float OuterRadius = 100;
 
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, meta = (EditCondition = "bIsAoe"))
-		bool bDebugVisualiseAoe = false;
+	float Falloff = 1;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, meta = (EditCondition = "bIsAoe"))
+	bool bDebugVisualiseAoe = false;
 
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, meta = (EditCondition = "bDebugVisualiseAoe"))
-		TSubclassOf<AActor> DebugVisClass;
-	
+	TSubclassOf<AActor> DebugVisClass;
+	FTimerHandle DetonationTimerHandle;
+
 	// Function that initializes the projectile's velocity in the shoot direction.
 	void InitVelocity(const FVector& ShootDirection);
 
 	UFUNCTION()
-		void OnCompHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit);
+	void OnCompHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp,
+	               FVector NormalImpulse, const FHitResult& Hit);
 
 	UFUNCTION()
-	void OnCompBeginOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+	void OnCompBeginOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp,
+	                        int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
 
 private:
-	void AoeDamage(const FHitResult& Hit);
+	void AoeDamage(const FVector& Location);
 	void PointDamage(AActor* OtherActor, const FHitResult& Hit);
+	void Detonate();
+
+	void BeginPlay() override;
 
 	UPROPERTY(VisibleAnywhere)
-		UStaticMeshComponent* MeshComp = nullptr;
+	UStaticMeshComponent* MeshComp = nullptr;
 
 	UPROPERTY(VisibleAnywhere, Category = Projectile)
-		USphereComponent* CollisionComp = nullptr;
+	USphereComponent* CollisionComp = nullptr;
 
 	UPROPERTY(VisibleAnywhere, Category = Movement)
-		UProjectileMovementComponent* ProjectileMovementComp;
-	
-	uint32 HeroControllerId;
+	UProjectileMovementComponent* ProjectileMovementComp;
 
+	uint32 HeroControllerId;
 };
